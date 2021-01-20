920dd544f1dbaecb2fff6abf73c6b160b436bc9f
meixin <meixinchoy@gmail.com>
2021-01-20 16:40:32 +0800

    added sign up validation

--- controllers/user.js
+++ controllers/user.js
@@ -2,9 +2,9 @@ let bcrypt = require("bcrypt");
 const passport = require('passport');
 require('../passport_setup')(passport);
 let flash = require('connect-flash');
-var UserModel = require("../models/userModel")
-//const { isEmpty } = require('lodash');
-//const { validateUser } = require('../validators/signup');
+var User = require("../models/userModel")
+const { isEmpty } = require('lodash');
+const { validateUser } = require('../validators/signup');
 
 exports.show_login = function (req, res, next) {
     res.render('user/login', { formData: {}, errors: {} });
@@ -14,29 +14,44 @@ exports.show_signup = function (req, res, next) {
     res.render('user/signup', { formData: {}, errors: {} });
 }
 
-// const rerender_signup = function (errors, req, res, next) {
-//     res.render('user/signup', { formData: req.body, errors: errors });
-// }
+const rerender_signup = function (errors, req, res, next) {
+    res.render('user/signup', { formData: req.body, errors: errors });
+}
+
 const generateHash = function (password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
 }
 
 exports.signup = function (req, res, next) {
-    var user = new User.UserModel({
-        email: req.body.email,
-        password: generateHash(req.body.password)
-    })
-    
-    user.save().exec( function (err, user) {
-        if (err) return done(err);
-            passport.authenticate('local', {
-            successRedirect: '/',
-            failureRedirect: '/signup',
-            failureFlash: true
-        })(req, res, next);
-        return done(null, user);
-    });
+    let errors = {};
+    return validateUser(errors, req).then(errors => {
+        if (!isEmpty(errors)) {
+            rerender_signup(errors, req, res, next);
+        } else {
+            var user = new User.UserModel({
+                email: req.body.email,
+                password: generateHash(req.body.password)
+            })
 
+            return user.save().then(result => {
+                passport.authenticate('local', {
+                    successRedirect: '/',
+                    failureRedirect: '/signup',
+                    failureFlash: true
+                })(req, res, next);
+            })
+
+            // user.save().exec(function (err, user) {
+            //     if (err) return done(err);
+            //     passport.authenticate('local', {
+            //         successRedirect: '/',
+            //         failureRedirect: '/signup',
+            //         failureFlash: true
+            //     })(req, res, next);
+            //     return done(null, user);
+            // });
+        }
+    })
 
     // try {
     //     const user = new UserModel.UserModel();
